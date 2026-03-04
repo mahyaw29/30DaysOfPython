0ce053802dd53e1d1ed8401d466ace6d39ac2558
Mahya <mahyaw29@gmail.com>
2026-03-04 13:59:22 -0600

    day 1

---
+++ day_1/helloworld.py
@@ -0,0 +1,37 @@
+# Day 1 - 30 Days of Python Challenge
+
+# LEVEL 1 & 2
+print(2+3)      # addition
+print(3-1)      # subtraction
+print(2*3)      # multiplication
+print(3%2)      # modulus
+print(3/2)      # division
+print(3**2)     # exponential
+print(3//2)     # floor division
+
+print('Mahya')      # Name
+print('Waddell')    # Last name
+print('USA')        # Country
+print('I am enjoying 30 days of python.')
+
+# Checking data types
+print(type(10))                 # integer
+print(type(9.8))                # float
+print(type(3.14))               # float
+print(type(4-4j))               # complex number
+print(type([1,2,3]))            # list
+print(type('Mahya'))            # string
+print(type('Waddell'))          # string
+print(type('USA'))              # string
+
+
+# LEVEL 3
+print(type(True))               # boolean
+print(type({'name':'Mahya'}))   # dictionary
+print(type({9.8,3.14,2.7}))     # set
+print(type((9.8,3.14,2.7)))     # tuple
+
+# Euclidean distance between (2,3) and (10,8)
+print(((2-10)**2)+((3-8)**2))
+
+
