bc4349eb652803fb969f0907c109892ac3bdbfab
Mahya <mahyaw29@gmail.com>
2026-04-28 13:06:40 -0500

    starting day 4 exercises

---
+++ day_4/string.py
@@ -0,0 +1,119 @@
+from operator import concat
+# task 1
+print("Task 1")
+title = "Thirty " + "Days " + "Of " + "Python"
+print(title)
+# task 2 & 3
+print("Task 2 & 3, no print")
+company = "Coding" + " For " + "All"
+# task 4
+print("Task 4")
+print(company)
+# task 5
+print("Task 5")
+print(len(company))
+# task 6
+print("Task 6")
+company_upper = company.upper()
+print(company_upper)
+# task 7
+print("Task 7")
+company_lower = company.lower()
+print(company_lower)
+# task 8
+print("Task 8")
+company_title = company.title()
+print(company_title)
+# task 9
+print("Task 9")
+company_swapcase = company.swapcase()
+print(company_swapcase)
+# task 10
+print("Task 10")
+company_slice = company[0:6]
+print(company_slice)
+# task 11
+print("Task 11")
+print("Company: ", company)
+company = company.replace("Coding", "Python")
+print("Company: ", company)
+# task 12
+print("Task 12")
+company = company.replace("All", "Everyone")
+print("Company: ", company)
+# task 13
+print("Task 13")
+company = "Coding For All"
+company_split = company.split()
+print(company_split)
+# task 14
+print("Task 14")
+company_list = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print("Original company list: ", company_list)
+company_list_split = company_list.split(", ")
+print("Split list: ", company_list_split)
+# task 15
+print("Task 15")
+print("Index 0: ", company[0])
+# task 16
+print("Task 16")
+print("Last Index: ", company[-1])
+# task 17
+print("Task 17")
+print("Index 10: ", company[9])  
+# task 18
+print("Task 18")
+string_18 = "Python For Everyone"
+print(string_18[0:7:15])
+# task 19
+print("Task 19")
+string_19 = "Coding For All"
+print(string_19[0:6:15])
+# task 20
+print("Task 20")
+first_c = string_19.find("C")
+print("First index of 'C': ", first_c)
+# task 21
+print("Task 21")
+first_f = string_19.find("F")
+print("First index of 'F': ", first_f)
+# task 22
+print("Task 22")
+string_22 = "Coding For All People"
+print(string_22.rfind("l"))
+# task 23
+print("Task 23")
+string_23 = "You cannot end a sentence with because because because is a conjunction"
+print(string_23.find("because"))
+# task 24
+print("Task 24")
+print(string_23.rindex("because"))
+# task 25
+print("Task 25")
+print(string_23[31:54])
+# task 26
+
+# task 27
+
+# task 28
+
+# task 29
+
+# task 30
+
+# task 31
+
+# task 32
+print("Task 32")
+
+# task 33
+print("Task 33")
+
+# task 34
+print("Task 34")
+
+# task 35
+print("Task 35")
+
+# task 36
+print("Task 36")
